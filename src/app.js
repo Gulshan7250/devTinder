423c9b7c1c1d0315fde032f2c0b1e9fe5d8898a8
Gulshan Kumar <gulshankumar725044@gmail.com>
2025-11-13 23:17:37 +0530

    playing with routes

--- src/app.js
+++ src/app.js
@@ -2,9 +2,16 @@ const express = require("express");
 
 const app = express();
 
-app.use("/test",(req, res) => {
-    res.send("Hello from the server");
-});
+// This will only handle GET call to /user
+app.get("/user/:userId/:name/:password", (req, res)=>{
+    console.log(req.params);
+    res.send({firstName: "Gulshan", lastName: "Yadav"});
+})
+
+// this will match all the HTTP method API calls to /
+app.use("/", (req, res)=>{
+    res.send("Namaste Gulshan");
+})
 
 app.listen(3000, ()=>{
     console.log("Server is successfully listening on port 3000....");
